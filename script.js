4de20efd876f4fbc3fdde3da374cb294b45d326b
Prakasith Noinala <44933709+2prak@users.noreply.github.com>
2022-01-23 03:10:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
 function generateTarget() {
   return Math.floor(Math.random() * 9);
 };
 
+// This finds the distance between two numbers and returns absolute of distance
+
 const getAbsoluteDistance = (number1, number2) => {
   return Math.abs(number1 - number2);
 };
+
+/*  This function checks if a valid number is input by user and alerts if not. It also calls the getAbsoluteDistance function 
+to determine distance and ompares the user and computer guesses to target and returns true if human is closer to target or false if not */
 
 const compareGuesses = (human, computer, target) => {
   if (human > 10) {
@@ -21,7 +27,9 @@ const compareGuesses = (human, computer, target) => {
     return false;
   };
 };
- 
+
+// This function updates the scores after each round for computer and user
+
 const updateScore = winner => {
   if (winner === 'human') {
     humanScore += 1;
@@ -30,6 +38,7 @@ const updateScore = winner => {
   };
 };
 
+// This function increase the round counter after each game
 const advanceRound = () => {
   currentRoundNumber += 1;
 };
